f8504359bdb364d721e1327fabe2e3c9a17f5975
pauljabernathy <pauljabernathy@gmail.com>
2019-10-13 17:14:30 -0400

    added duplicate entry options to simple_binary_insert

--- weighted_binary_tree.py
+++ weighted_binary_tree.py
@@ -1,6 +1,6 @@
 
-#DuplicateEntryOption neews to be defined before WeightedBinaryTree in order to use it in the method parameters
-#InsertionResult can be defined after WeightedBinaryTree if it is not used in the parameters.
+# DuplicateEntryOption needs to be defined before WeightedBinaryTree in order to use it in the method parameters
+# InsertionResult can be defined after WeightedBinaryTree if it is not used in the parameters.
 class DuplicateEntryOption:
     ADD = "Add"
     REPLACE = "Replace"
@@ -26,10 +26,8 @@ class WeightedBinaryTree(object):
             self.display('')
             return
         if prefix == '':
-            #print(f'{self.key} {self.weight} {self.sub_tree_weight} {self.get_tree_weight()}')
             print(str(self))
         else:
-            #print(f'{prefix} {self.key} {self.weight} {self.sub_tree_weight} {self.get_tree_weight()}')
             print(f'{prefix} ' + str(self))
         if self.left is not None:
             self.left.display(prefix + 'l')
@@ -39,11 +37,10 @@ class WeightedBinaryTree(object):
     def get_tree_weight(self):
         return self.weight + self.sub_tree_weight
 
-    #TODO:  Make a DuplcateEntryOption class, for now just default to replace
     def insert(self, key, weight=DEFAULT_WEIGHT):
         self.simple_binary_insert(key, weight)
 
-    def simple_binary_insert(self, key, weight=DEFAULT_WEIGHT, duplicate_entry_option=DuplicateEntryOption.ADD):
+    def simple_binary_insert(self, key, new_weight=DEFAULT_WEIGHT, duplicate_entry_option=DuplicateEntryOption.ADD):
         if key is None:
             result = InsertionResult(None)
             result.status = InsertionResultStatus.FAILED
@@ -51,20 +48,34 @@ class WeightedBinaryTree(object):
 
         result = InsertionResult()
         if key == self.key:
-            self.weight = weight
             result.inserted_node = self
+            if duplicate_entry_option == DuplicateEntryOption.IGNORE:
+                result.status = InsertionResultStatus.IGNORED
+            elif duplicate_entry_option == DuplicateEntryOption.REPLACE:
+                result.previous_weight = self.weight
+                self.weight = new_weight
+                result.status = InsertionResultStatus.REPLACED
+            elif duplicate_entry_option == DuplicateEntryOption.ADD:
+                result.previous_weight = self.weight
+                self.weight += new_weight
+                result.status = InsertionResultStatus.REPLACED
+            else:
+                # Default behavior will be the same as ignore
+                # TODO: specify a default as a constant or a class or object variable, not buried in the code here!
+                result.status = InsertionResultStatus.IGNORED
+
         elif key < self.key:
             if self.left is not None:
-                result = self.left.simple_binary_insert(key, weight)
+                result = self.left.simple_binary_insert(key, new_weight, duplicate_entry_option)
             else:
-                self.left = WeightedBinaryTree(key, weight)
+                self.left = WeightedBinaryTree(key, new_weight)
                 result.inserted_node = self.left
                 result.status = InsertionResultStatus.CREATED
         elif key > self.key:
             if self.right is not None:
-                result = self.right.simple_binary_insert(key, weight)
+                result = self.right.simple_binary_insert(key, new_weight, duplicate_entry_option)
             else:
-                self.right = WeightedBinaryTree(key, weight)
+                self.right = WeightedBinaryTree(key, new_weight)
                 result.inserted_node = self.right
                 result.status = InsertionResultStatus.CREATED
         self.update_sub_tree_weights()
@@ -80,6 +91,9 @@ class InsertionResult:
     def __init__(self, inserted_node:WeightedBinaryTree=None):
         self.inserted_node = inserted_node
         self.status = InsertionResultStatus.UNKOWNN
+        self.previous_weight = 0.0
+
+    # TODO: @property attributes?
 
 
 class InsertionResultStatus:
--- weighted_binary_tree_test.py
+++ weighted_binary_tree_test.py
@@ -2,13 +2,24 @@ import unittest
 from weighted_binary_tree import *
 
 
-class BasicInsertionTestNoDuplication(unittest.TestCase):
+class TestCreateTree(unittest.TestCase):
+
+    def test_create_tree(self):
+        wbt = WeightedBinaryTree('root')
+        wbt.left = WeightedBinaryTree('left', 2)
+        wbt.right = WeightedBinaryTree('z')
+        self.assertTrue(isinstance(wbt, WeightedBinaryTree))
+        self.assertTrue(isinstance(wbt.left, WeightedBinaryTree))
+        self.assertTrue(isinstance(wbt.right, WeightedBinaryTree))
 
     def test_display(self):
         wbt = WeightedBinaryTree('root')
         wbt.left = WeightedBinaryTree('left', 2)
         wbt.right = WeightedBinaryTree('z')
         #wbt.display()
+
+
+class BasicInsertionTestNoDuplication(unittest.TestCase):
 
     def test_simple_binary_insert_puts_items_in_the_correct_spot(self):
         #With simple_binary_insert, k will remain the root
@@ -22,11 +33,17 @@ class BasicInsertionTestNoDuplication(unittest.TestCase):
 
         root.display()
         self.assertEqual('c', root.left.key)
+        self.assertEqual(1.0, root.left.weight)
         self.assertEqual('w', root.right.key)
+        self.assertEqual(1.0, root.right.weight)
         self.assertEqual('m', root.right.left.key)
+        self.assertEqual(1.0, root.right.left.weight)
         self.assertEqual('x', root.right.right.key)
+        self.assertEqual(1.0, root.right.right.weight)
         self.assertEqual('a', root.left.left.key)
+        self.assertEqual(1.0, root.left.loeft.weight)
         self.assertEqual('e', root.left.right.key)
+        self.assertEqual(1.0, root.left.weight)
 
     def test_simple_binary_insert_returns_correct_insertion_result(self):
         root = WeightedBinaryTree('k')
@@ -40,6 +57,75 @@ class BasicInsertionTestNoDuplication(unittest.TestCase):
         self.assertEqual('w', result.inserted_node.key)
         self.assertEqual(InsertionResultStatus.CREATED, result.status)
 
+
+class BasicInsertionTestWithReplacement(unittest.TestCase):
+
+    def test_simple_binary_insert_returns_replaced_status(self):
+        root = WeightedBinaryTree('k')
+        root.simple_binary_insert('c', 1.0, DuplicateEntryOption)
+        root.simple_binary_insert('w', 1.0, DuplicateEntryOption.REPLACE)
+
+        result = root.simple_binary_insert('c', 3.0, DuplicateEntryOption.REPLACE)
+        self.assertEqual('Replaced', result.status)
+        result = root.simple_binary_insert('w', 3.0, DuplicateEntryOption.REPLACE)
+        self.assertEqual('Replaced', result.status)
+
+    def test_simple_binary_insert_replaces_items_that_should_be_replaced(self):
+        # With simple_binary_insert, k will remain the root
+        root = WeightedBinaryTree('k')
+        root.simple_binary_insert('c', 1.0, DuplicateEntryOption.REPLACE)
+        root.simple_binary_insert('w', 1.0, DuplicateEntryOption.REPLACE)
+        root.simple_binary_insert('m', 1.0, DuplicateEntryOption.REPLACE)
+
+        result = root.simple_binary_insert('c', 3.0, DuplicateEntryOption.REPLACE)
+        self.assertEqual('Replaced', result.status)
+        root.simple_binary_insert('w', 3.0, DuplicateEntryOption.REPLACE)
+        root.simple_binary_insert('m', 47.0, DuplicateEntryOption.REPLACE)
+
+        self.assertEqual('c', root.left.key)
+        self.assertEqual(3.0, root.left.weight)
+        self.assertEqual('w', root.right.key)
+        self.assertEqual(3.0, root.right.weight)
+        self.assertEqual('m', root.right.left.key)
+        self.assertEqual(47.0, root.right.left.weight)
+
+
+class BasicInsertionTestWithIgnore(unittest.TestCase):
+
+    def test_simple_binary_insert_returns_ignored_status(self):
+        root = WeightedBinaryTree('k')
+        root.simple_binary_insert('c', 1.0, DuplicateEntryOption)
+        root.simple_binary_insert('w', 1.0, DuplicateEntryOption.IGNORE)
+
+        result = root.simple_binary_insert('c', 3.0, DuplicateEntryOption.IGNORE)
+        self.assertEqual('Ignored', result.status)
+        result = root.simple_binary_insert('w', 3.0, DuplicateEntryOption.IGNORE)
+        self.assertEqual('Ignored', result.status)
+
+    def test_simple_binary_insert_ignores_items_that_should_be_ignored(self):
+        # With simple_binary_insert, k will remain the root
+        root = WeightedBinaryTree('k')
+        root.simple_binary_insert('c', 1.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('w', 1.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('m', 1.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('x', 1.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('a', 1.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('e', 1.0, DuplicateEntryOption.IGNORE)
+
+        root.simple_binary_insert('c', 3.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('w', 3.0, DuplicateEntryOption.IGNORE)
+        root.simple_binary_insert('m', 47.0, DuplicateEntryOption.IGNORE)
+
+        self.assertEqual('c', root.left.key)
+        self.assertEqual(1.0, root.left.weight)
+        self.assertEqual('w', root.right.key)
+        self.assertEqual(1.0, root.right.weight)
+        self.assertEqual('m', root.right.left.key)
+        self.assertEqual(1.0, root.right.left.weight)
+
+
+class TestHandleSubTrees(unittest.TestCase):
+
     def test_update_sub_tree_weight(self):
         root = WeightedBinaryTree('k', 2.0)
         #simple_binary_insert() calls update_sub_tree_weights, so add the nods manually
@@ -52,24 +138,3 @@ class BasicInsertionTestNoDuplication(unittest.TestCase):
         self.assertEqual(3.0, root.sub_tree_weight)
         root.update_sub_tree_weights()
         self.assertEqual(7.0, root.sub_tree_weight)
-
-
-class BasicInsertionTestWithReplacement(unittest.TestCase):
-
-    def test_simple_binary_insert_puts_items_in_the_correct_spot(self):
-        # With simple_binary_insert, k will remain the root
-        root = WeightedBinaryTree('k')
-        root.simple_binary_insert('c', 1.0, DuplicateEntryOption.REPLACE)
-        root.simple_binary_insert('w', 1.0, DuplicateEntryOption.REPLACE)
-        root.simple_binary_insert('m', 1.0, DuplicateEntryOption.REPLACE)
-        root.simple_binary_insert('x', 1.0, DuplicateEntryOption.REPLACE)
-        root.simple_binary_insert('a', 1.0, DuplicateEntryOption.REPLACE)
-        root.simple_binary_insert('e', 1.0, DuplicateEntryOption.REPLACE)
-
-        root.display()
-        self.assertEqual('c', root.left.key)
-        self.assertEqual('w', root.right.key)
-        self.assertEqual('m', root.right.left.key)
-        self.assertEqual('x', root.right.right.key)
-        self.assertEqual('a', root.left.left.key)
-
